9a6529c7bb6975c9c107913f04d9e731c0dd0b34
chenxuuu <liucx1@qq.com>
2017-05-12 21:09:12 +0800

    初版测试

---
+++ addOneSecond/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace addOneSecond
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DispatcherTimer timer;//定义定时器
+        public MainPage()
+        {
+            this.InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += Timer_Tick;//每秒触发这个事件，以刷新时间
+            timer.Start();
+        }
+
+        private async void Timer_Tick(object sender, object e)
+        {
+            string result;
+            string allSecondsString = await webLib.HttpGet("https://angry.im/l/life");  //获取秒数
+            long allSeconds = long.Parse(allSecondsString);   //转换成long
+            long dd, mm, hh, ss;     //用于存储最终数值
+            dd = allSeconds / 60 / 60 / 24;
+            hh = allSeconds / 60 / 60 % 24;
+            mm = allSeconds / 60 % 60;
+            ss = allSeconds % 60;
+            result = $"{dd}d {hh}h {mm}m {ss}s";
+            secondsShow.Text = result;  //显示结果
+        }
+
+        private async void secondGet_Click(object sender, RoutedEventArgs e)
+        {
+            await webLib.HttpPost("https://angry.im/p/life", "+1s");
+        }
+    }
+}
---
+++ addOneSecond/lib/WebLib.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class webLib {
+    public static async Task<string> HttpPost(string url, string postDataStr, string encode = "utf-8", CookieCollection cc = null) {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.CookieContainer = new CookieContainer();
+        if (cc != null) {
+            request.CookieContainer.Add(new Uri(url), cc);
+        }
+        //设置请求方式为POST
+        request.Method = "POST";
+        //在POST里一定要注意写入Content—Length，这里的长度是指POST上传的数据的长度，可以使用Encoding中的GetByteCount方法完成
+        request.Headers["Content-Length"] = Encoding.UTF8.GetByteCount(postDataStr).ToString();
+        //ContentType设置为Web表单模式
+        request.ContentType = "application/x-www-form-urlencoded";
+
+        //test request header
+        //request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+        //request.Headers["Accept-Encoding"] = "gzip, deflate";
+        //request.Headers["Accept-Language"] = "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3";
+        //request.Headers["Connection"] = "keep-alive";
+        //request.Headers["DNT"] = "1";
+        //request.Headers["Host"] = "10.3.8.211";
+        //request.Headers["Referrer"] = "http://10.3.8.211/";
+        //request.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0";
+
+        //拿到request的输入流
+        Stream myRequestStream = await request.GetRequestStreamAsync();
+
+        //use this function to register the encoding machine or it will throw expect Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        //I Don't know why it doesn't work, so I use direct function to write
+        //StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding(encode));
+        //myStreamWriter.Write(postDataStr);
+
+        //将传输的数据转化为ASCII码写入输入流
+        byte[] bs = Encoding.ASCII.GetBytes(postDataStr);
+        myRequestStream.Write(bs, 0, bs.Length);
+
+        //异步得到Response并且将Response转换为String
+        HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+        Stream myResponseStream = response.GetResponseStream();
+        StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding(encode));
+        string retString = myStreamReader.ReadToEnd();
+        return retString;
+    }
+    public static async Task<string> HttpGet(string url, string getDataStr = null, string encode = "utf-8",CookieCollection cc = null) {
+        //Get方式提交数据只需要在网址后面使用？即可，如果多组数据，需要在提交的时候使用&连接
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + (getDataStr == null ? "" : "?") + getDataStr);
+        //将Cookie写入
+        request.CookieContainer = new CookieContainer();
+        if (cc != null) {
+            request.CookieContainer.Add(new Uri(url), cc);
+        }
+        //设置request的方式为GET
+        request.Method = "GET";
+        //设置HTTP头的内容类型,如果需要在Http头中加入其他内容，可以直接使用 request.Headers["头名称"]="头内容" 来添加
+        request.ContentType = "text/html;charset=UTF-8";
+        //通过异步方法拿到回应
+        HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+        //写入流
+        Stream myResponseStream = response.GetResponseStream();
+        //注册编码转换器（这里同之前WPF开发中不同，需要事先注册编码转换器才能使用）
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        //进行内容编码转换
+        StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding(encode));
+        //将转换后的内容转化为字符串并返回
+        string retString = myStreamReader.ReadToEnd();
+        return retString;
+    }
+
+}
