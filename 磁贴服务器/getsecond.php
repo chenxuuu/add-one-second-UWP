d606aad5d028aae1556e672ae7b01fa2884217fd
chenxuuu <liucx1@qq.com>
2017-05-25 10:17:31 +0800

    磁贴图片扔到软件里，省点流量

--- 磁贴服务器/getsecond.php
+++ 磁贴服务器/getsecond.php
@@ -104,7 +104,7 @@ $resultstr = random_str();
 <tile>
   <visual version="2" branding="logo" displayName="+1s">
     <binding template="TileSquare150x150PeekImageAndText01" fallback="TileSquarePeekImageAndText01">
-      <image id="1" src="http://open.papapoi.com/excited/<?php echo $result;?>.png" alt="alt text"/>
+      <image id="1" src="ms-appx:///Assets/pic/<?php echo $result;?>.png" alt="alt text"/>
       <text id="1">时间众筹总计</text>
       <text id="2"><?php echo $dd.'天';?></text>
       <text id="3"><?php echo $hh.'小时';?></text>
